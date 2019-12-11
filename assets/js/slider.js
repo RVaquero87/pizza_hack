2625a0b4bcd2e7b14c5385199af3b9afed43cf12
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-12-11 01:15:11 +0100

    html messages sucess - game over - sucess end y game

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,10 +1,18 @@
 window.onload = function() {
   let swiper = new Swiper('.slider-ingredientes', {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-      el: '.slider-ingredientes-pagination',
-      clickable: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    preventClicks: !1,
+    breakpoints: {
+      960: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 }
