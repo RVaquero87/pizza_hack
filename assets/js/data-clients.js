4e726b62838394b48aab6eab7634b4f969404f00
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-12-12 01:18:42 +0100

    data clients ok

---
+++ assets/js/data-clients.js
@@ -0,0 +1,82 @@
+let clients = [
+  {
+    nameClient: 'Vivaldi',
+    srcClient: 'assets/img/cliente/cliente-vivaldi.png',
+    namePizza: 'Cuatro Estaciones',
+    ingredients: ['sal', 'aceite', 'tomate-frito', 'tomate', 'mozarella', 'alcachofas', 'aceitunas-negras', 'jamon', 'setas', 'pimienta-negra'],
+    clues: [
+      {
+        easyClues: ['Tiene alimentos de las 4 estaciones', 'Cuatro Estaciones como mi grupo de conciertos'],
+        hardClues: ['Si estás indeciso, está es tu pizza', 'Es perfecta para cualquier estación'],
+      }
+    ],
+  },
+  {
+    nameClient: 'Moana',
+    srcClient: 'assets/img/cliente/cliente-moana.png',
+    namePizza: 'Hawaiana',
+    ingredients: ['sal', 'aceite', 'tomate-frito', 'pina', 'queso-rallado', 'jamon', 'oregano'],
+    clues: [
+      {
+        easyClues: ['Se creo en Canadá pero todos piensan que son de mi tierra', 'Se piensa que son de Hawai', 'Hawaiana como yo'],
+        hardClues: ['Se le adjudica al cocinero Sam Panopoulos', 'Se creó en Satelli Restaurant en Ontario, Canadá', 'Se creo en 1962', 'Muy oidada o muy querida, no deja indiferente a nadie'],
+      }
+    ],
+  },
+  {
+    nameClient: 'Michelangelo',
+    srcClient: 'assets/img/cliente/cliente-tortuga.png',
+    namePizza: 'Margherita',
+    ingredients: ['sal', 'aceite', 'tomate-frito', 'tomate', 'ajo', 'mozarella', 'albahaca', 'pimienta-negra', 'oregano'],
+    clues: [
+      {
+        easyClues: ['Se llama así por la reina Margherita di Savoia', 'Sus ingredientes representan los colores de la bandera italiana', 'Margherita es su nombre'],
+        hardClues: ['Origen Napolitano', 'Clásica Pizza Italiana', 'Es la pizza de las reinas', 'Surgio en 1889 en el Palacio de Capodimonte', 'Se le atribuye al cocinero Raffaele Esposito'],
+      }
+    ],
+  },
+  {
+    nameClient: 'Heidi',
+    namePizza: 'Primavera',
+    ingredients: ['sal', 'aceite', 'tomate-frito', 'pimiento', 'maiz', 'queso', 'tomate', 'oregano', 'huevo-duro'],
+    clues: [
+      {
+        easyClues: ['Verano, Otoño, Invierno y...', 'Las gallinas nos ayudan con un ingrediente', 'La encontrarás en los restaurantes como Primavera'],
+        hardClues: ['Muy tradicional', 'Evoca los colores primaverales'],
+      }
+    ],
+  },
+  {
+    nameClient: 'Mario Bros',
+    namePizza: 'Funghi',
+    ingredients: ['sal', 'aceite', 'tomate-frito', 'queso', 'setas', 'jamon', 'mozarella', 'oregano'],
+    clues: [
+      {
+        easyClues: ['Su protagonista son las setas', 'Su nombre significa hongos', 'Su ingrediente especial me hace crecer mucho más', 'La llaman Funghi'],
+        hardClues: ['Su ingrediente especial me hace crecer mucho más', 'Me recuerda a Otoño', 'Es de origen Napolitano', 'Para paladares con devoción por los sabores auténticos'],
+      }
+    ],
+  },
+  {
+    nameClient: 'Speedy Gonzales',
+    namePizza: 'Diavola',
+    ingredients: ['sal', 'aceite', 'mozarella', 'salami', 'tomate-frito', 'tomate', 'albahaca', 'oregano'],
+    clues: [
+      {
+        easyClues: ['El picante es su toque especial', 'A veces se usa Chorizo pero muy picante', 'Me encanta la pizza picante', 'Diavola es su nombre'],
+        hardClues: ['Es la típica pizza italiana', 'No es apta para todo el mundo', 'Nos gusta a los Méxicanos','Es una de las pizzas más vendidas en latinoámerica y Países Bajos'],
+      }
+    ],
+  },
+  {
+    nameClient: 'Paris Hilton',
+    namePizza: 'Caprichosa',
+    ingredients: ['sal', 'aceite', 'mozarella', 'huevo-duro', 'anchoas','aceitunas-negras', 'albahaca', 'setas', 'alcachofas'],
+    clues: [
+      {
+        easyClues: ['Es una pizza Caprichosa, como yo'],
+        hardClues: ['Pizza para gente exigente', 'Junto a la Margarita, las 4 estaciones y la 4 quesos una de las tradicionales','Es una explosión de sabores'],
+      }
+    ],
+  },
+]
